c7d8565d103d4bb51d791f5b0eaf87a8ec883764
Andi Emma Davies <andidavies92@outlook.com>
2019-05-11 21:29:14 +0100

    Tidy-up build process and root directory

---
+++ includes/Start Location Array - Ending.asm
@@ -0,0 +1,13 @@
+; ---------------------------------------------------------------------------
+; Ending start location array
+; ---------------------------------------------------------------------------
+
+		incbin	"starts\ghz1 (Credits demo 1).bin"
+		incbin	"starts\mz2 (Credits demo).bin"
+		incbin	"starts\syz3 (Credits demo).bin"
+		incbin	"starts\lz3 (Credits demo).bin"
+		incbin	"starts\slz3 (Credits demo).bin"
+		incbin	"starts\sbz1 (Credits demo).bin"
+		incbin	"starts\sbz2 (Credits demo).bin"
+		incbin	"starts\ghz1 (Credits demo 2).bin"
+		even
---
+++ includes/Start Location Array - Special Stages.asm
@@ -0,0 +1,11 @@
+; ---------------------------------------------------------------------------
+; Special stage start locations
+; ---------------------------------------------------------------------------
+
+		incbin	"starts\ss1.bin"
+		incbin	"starts\ss2.bin"
+		incbin	"starts\ss3.bin"
+		incbin	"starts\ss4.bin"
+		incbin	"starts\ss5.bin"
+		incbin	"starts\ss6.bin"
+		even
